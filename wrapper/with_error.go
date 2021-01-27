52d202a420af4e00929581880397d4f5a1b20d9c
Hilari Moragrega <hilari@hilarimoragrega.com>
2021-01-27 02:03:09 +0100

    improve doc

--- wrapper/with_error.go
+++ wrapper/with_error.go
@@ -8,6 +8,9 @@ import (
 
 // WithError is a job wrapper that allows to use jobs
 // that return errors.
+//
+// The second parameter is a callback that will be
+// called with the result of the job.
 func WithError(job func(context.Context) error, result func(error)) workers.Job {
 	return workers.JobFunc(func(ctx context.Context) {
 		result(job(ctx))
