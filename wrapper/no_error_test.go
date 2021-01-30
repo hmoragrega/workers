db1803b6fb252e6f4019e622fc489d0eb47995f5
Hilari Moragrega <hilari@hilarimoragrega.com>
2021-01-30 13:18:14 +0100

    added wrappers again

---
+++ wrapper/no_error_test.go
@@ -0,0 +1,15 @@
+package wrapper
+
+import (
+	"context"
+
+	"github.com/hmoragrega/workers"
+)
+
+// NoError wraps a job returning always nil
+func NoError(job func(ctx context.Context)) workers.Job {
+	return workers.JobFunc(func(ctx context.Context) error {
+		job(ctx)
+		return nil
+	})
+}
